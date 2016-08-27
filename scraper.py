7efdab5b22d113007cb66fec31dfdc9ac9e45c91
jooste64 <dajoosten@gmail.com>
2016-08-27 11:21:39 +0200

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -5,78 +5,78 @@
 # Updated December 2012. The G-INFO website requires at least a two letter
 # prefix. We have to search for G-AA?? rather than G-A??? now.
 
-+# Pull information from the CAA's G-INFO database of UK registered aircraft.
- +# http://www.caa.co.uk/application.aspx?catid=60&pagetype=65&appid=1
- +# Richard Cameron. May 2011
- +
- +# Updated December 2012. The G-INFO website requires at least a two letter
- +# prefix. We have to search for G-AA?? rather than G-A??? now.
- +
- +import re, datetime, urllib2, time
- +
- +import scraperwiki
- +import lxml.html
- +from lxml import etree
- +def scrape(url):
- +    N=10
- +    for i in range(N+1):
- +        try:
- +            return scraperwiki.scrape(url, user_agent="Mozilla/5.0 (Windows NT 6.2; Win64; x64; rv:16.0.1) Gecko/20121011 Firefox/16.0.1")
- +        except Exception, e:
- +            print "Failed to fetch", url, e
- +            print "Retry", i
- +            if i==N:
- +                raise    
- +
- +def current_prefix():
- +    # Spider aircraft based on the first letters of their registration.
- +    # On the first run we'll do G-AAA?, next run we'll do G-AAB?, and so on
- +    # Need to keep track of which prefix we're on between runs
- +    return scraperwiki.sqlite.get_var("current_prefix", "AAA")
- +
- +def next_prefix(x):
- +    """ Map AAA -> AAB -> AAC -> ... -> AAZ -> ABA -> ... -> ZZZ -> AAA """
- +    if not re.match(r'[A-Z]{3}$', x):
- +        raise ValueError, "Bad prefix: %s" % x
- +    def aord(x):
- +        return ord(x) - ord('A')
- +    
- +    offset = 26*26*aord(x[0]) + 26*aord(x[1]) + aord(x[2])
- +    offset = (offset + 1) % (26**3)
- +    
- +    third = (offset % 26)
- +    offset = (offset - third) / 26
- +    second = offset % 26
- +    first = (offset - second) / 26
- +
- +    def achr(x):
- +        return chr( ord('A') + x)
- +
- +    return achr(first) + achr(second) + achr(third)
- +
- +
- +
- +
- +def fetch_registrations(url):
- +    """Spider the G-INFO side and yield a bunch of four_letter_registration
- +    registrations of (hopefully) all the aircraft registred"""
- +
- +
- +    while url:
- +        print "Fetching ", url
- +        html = scrape(url)
- +        root = lxml.html.fromstring(html)
- +        root.make_links_absolute("http://publicapps.caa.co.uk/modalapplication.aspx")
- +        for link in root.cssselect('tr td a[id^="currentModule_currentModule_myRepeater__ct"]'):
- +            reg = link.text_content().strip()
- +            if not re.match(r'G-[A-Z]{4}', reg):
- +                raise ValueError, "Bad registration: %s" % reg
- +            yield reg[2:]
- +
- +        # If this is the unique aircraft with this prefix, they might have redirected us
- +        # to the detail page already
- +        # We'd see something like this <span id="currentModule_currentModule_Registration" class="small">G-UACA</span>
- +        for span in root.cssselect('span[id^="currentModule_currentModule_Registration"]'):
+# Pull information from the CAA's G-INFO database of UK registered aircraft.
+ # http://www.caa.co.uk/application.aspx?catid=60&pagetype=65&appid=1
+ # Richard Cameron. May 2011
+ 
+ # Updated December 2012. The G-INFO website requires at least a two letter
+ # prefix. We have to search for G-AA?? rather than G-A??? now.
+ 
+ import re, datetime, urllib2, time
+ 
+ import scraperwiki
+ import lxml.html
+ from lxml import etree
+ def scrape(url):
+     N=10
+     for i in range(N+1):
+         try:
+             return scraperwiki.scrape(url, user_agent="Mozilla/5.0 (Windows NT 6.2; Win64; x64; rv:16.0.1) Gecko/20121011 Firefox/16.0.1")
+         except Exception, e:
+             print "Failed to fetch", url, e
+             print "Retry", i
+             if i==N:
+                 raise    
+ 
+ def current_prefix():
+     # Spider aircraft based on the first letters of their registration.
+     # On the first run we'll do G-AAA?, next run we'll do G-AAB?, and so on
+     # Need to keep track of which prefix we're on between runs
+     return scraperwiki.sqlite.get_var("current_prefix", "AAA")
+ 
+ def next_prefix(x):
+     """ Map AAA -> AAB -> AAC -> ... -> AAZ -> ABA -> ... -> ZZZ -> AAA """
+     if not re.match(r'[A-Z]{3}$', x):
+         raise ValueError, "Bad prefix: %s" % x
+     def aord(x):
+         return ord(x) - ord('A')
+     
+     offset = 26*26*aord(x[0]) + 26*aord(x[1]) + aord(x[2])
+     offset = (offset + 1) % (26**3)
+     
+     third = (offset % 26)
+     offset = (offset - third) / 26
+     second = offset % 26
+     first = (offset - second) / 26
+ 
+     def achr(x):
+         return chr( ord('A') + x)
+ 
+     return achr(first) + achr(second) + achr(third)
+ 
+ 
+ 
+ 
+ def fetch_registrations(url):
+     """Spider the G-INFO side and yield a bunch of four_letter_registration
+     registrations of (hopefully) all the aircraft registred"""
+ 
+ 
+     while url:
+         print "Fetching ", url
+        html = scrape(url)
+        root = lxml.html.fromstring(html)
+        root.make_links_absolute("http://publicapps.caa.co.uk/modalapplication.aspx")
+        for link in root.cssselect('tr td a[id^="currentModule_currentModule_myRepeater__ct"]'):
+            reg = link.text_content().strip()
+            if not re.match(r'G-[A-Z]{4}', reg):
+               raise ValueError, "Bad registration: %s" % reg
+            yield reg[2:]
+
+       # If this is the unique aircraft with this prefix, they might have redirected us
+        # to the detail page already
+       # We'd see something like this <span id="currentModule_currentModule_Registration" class="small">G-UACA</span>
+       for span in root.cssselect('span[id^="currentModule_currentModule_Registration"]'):
  +            reg = span.text
  +            if not re.match(r'G-[A-Z]{4}', reg):
  +                raise ValueError, "Bad registration: %s" % reg
